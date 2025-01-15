14b64369806f2145612a668e2343da22f2c8e3d4
misterlink19 <amlemartinezmarte@gmail.com>
2025-01-15 22:52:38 +0100

    Terminando con los cambios en src/main

--- src/main/java/com/lauracercas/moviecards/controller/CardController.java
+++ src/main/java/com/lauracercas/moviecards/controller/CardController.java
@@ -7,6 +7,8 @@ import com.lauracercas.moviecards.service.actor.ActorService;
 import com.lauracercas.moviecards.service.card.CardService;
 import com.lauracercas.moviecards.service.movie.MovieService;
 import com.lauracercas.moviecards.util.Messages;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-
 /**
  * Autor: Laura Cercas Ramos
  * Proyecto: TFM Integración Continua con GitHub Actions
@@ -23,10 +24,10 @@ import java.util.List;
  */
 @Controller
 public class CardController {
-
-
+    
+    @Autowired
+    ActorService actorService;
     private final MovieService movieService;
-    private final ActorService actorService;
     private final CardService cardService;
 
     public CardController(MovieService movieService, ActorService actorService, CardService cardService) {
